063e1c30efeda2ce7539c5f8d9e7f8c0e21c5722
Khánh Duy <97621507+Khanhdii@users.noreply.github.com>
2024-05-07 02:08:26 +0700

    app.py

--- app.py
+++ app.py
@@ -5,18 +5,6 @@ import numpy as np
 import tensorflow as tf
 from tempfile import NamedTemporaryFile
 
-# Attempt to import sounddevice and handle the ImportError if it occurs
-try:
-    import sounddevice as sd
-    SD_AVAILABLE = True
-except ImportError:
-    SD_AVAILABLE = False
-    st.error("SoundDevice is not available on this server.")
-
-# Import soundfile conditionally since it might be used only for recording
-if SD_AVAILABLE:
-    import soundfile as sf
-
 # Load the pre-trained model (ensure the model path is correct)
 loaded_model = tf.keras.models.load_model('voice_detecting_model.h5')
 
@@ -45,26 +33,6 @@ if audio_input == "Upload":
         st.audio(audio_file, format='audio/wav')
         with NamedTemporaryFile(delete=True, suffix='.wav') as temp_file:
             temp_file.write(audio_file.getvalue())
-            mfcc_features = extract_mfcc(temp_file.name)
-
-elif audio_input == "Record" and SD_AVAILABLE:
-    record_button = st.button("Record Audio")
-    if record_button:
-        # Record audio from microphone
-        st.info("Recording... Say something!")
-        audio_data = sd.rec(int(5 * 16000), samplerate=16000, channels=1, dtype='float32')
-        sd.wait()
-        st.info("Recording complete!")
-
-        gain_factor = 100.0  # Adjust this value to increase the volume
-        audio_data = audio_data * gain_factor  # Apply gain to increase volume
-        audio_data = np.clip(audio_data, -1.0, 1.0)
-
-        # Save audio to a file
-        with NamedTemporaryFile(delete=True, suffix='.wav') as temp_file:
-            sf.write(temp_file.name, audio_data, 16000)
-            # Display the recorded audio
-            st.audio(temp_file.name, format='audio/wav')
             mfcc_features = extract_mfcc(temp_file.name)
 
 # Model prediction and display results
