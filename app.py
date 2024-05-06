f54d01354da8ad3c4190a22875e188fb3decef85
Khánh Duy <97621507+Khanhdii@users.noreply.github.com>
2024-05-07 02:26:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def extract_mfcc(filename, n_mfcc=13):
 st.title("Voice Recognition App")
 
 # Allow the user to choose between uploading or recording audio
-audio_input = st.radio("Select audio input type:", (["Upload"]))
+audio_input = st.radio("Select audio input type:", ("Upload", ""))
 
 if audio_input == "Upload":
     audio_file = st.file_uploader("Upload an audio file", type=["wav", "mp3"])
