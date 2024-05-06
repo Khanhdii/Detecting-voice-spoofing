ac7ffb43705a5054dfd6a24bbfa10ddb03a60120
Khánh Duy <97621507+Khanhdii@users.noreply.github.com>
2024-05-07 02:01:42 +0700

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import streamlit as st
-import sounddevice as sd  # Correctly import sounddevice
 import torchaudio
 import torchaudio.transforms as T
 import numpy as np
-import soundfile as sf
 import tensorflow as tf
+from tempfile import NamedTemporaryFile
+
+# Attempt to import sounddevice and handle the ImportError if it occurs
+try:
+    import sounddevice as sd
+    SD_AVAILABLE = True
+except ImportError:
+    SD_AVAILABLE = False
+    st.error("SoundDevice is not available on this server.")
+
+# Import soundfile conditionally since it might be used only for recording
+if SD_AVAILABLE:
+    import soundfile as sf
 
 # Load the pre-trained model (ensure the model path is correct)
 loaded_model = tf.keras.models.load_model('voice_detecting_model.h5')
@@ -17,10 +28,9 @@ def extract_mfcc(filename, n_mfcc=13):
         melkwargs={'n_fft': 400, 'hop_length': 160, 'n_mels': 23}
     )
     mfcc = mfcc_transform(waveform).squeeze(0).detach().numpy()
-
     if mfcc.shape[1] < 500:
         mfcc = np.pad(mfcc, ((0, 0), (0, 500 - mfcc.shape[1])), mode='constant', constant_values=0)
-    return mfcc[:,:500]
+    return mfcc[:, :500]
 
 # Streamlit app interface
 st.title("Voice Recognition App")
@@ -33,9 +43,11 @@ if audio_input == "Upload":
     if audio_file is not None:
         # Process the uploaded audio file
         st.audio(audio_file, format='audio/wav')
-        mfcc_features = extract_mfcc(audio_file)
+        with NamedTemporaryFile(delete=True, suffix='.wav') as temp_file:
+            temp_file.write(audio_file.getvalue())
+            mfcc_features = extract_mfcc(temp_file.name)
 
-elif audio_input == "Record":
+elif audio_input == "Record" and SD_AVAILABLE:
     record_button = st.button("Record Audio")
     if record_button:
         # Record audio from microphone
@@ -46,18 +58,14 @@ elif audio_input == "Record":
 
         gain_factor = 100.0  # Adjust this value to increase the volume
         audio_data = audio_data * gain_factor  # Apply gain to increase volume
-
-        # Ensure the audio does not clip
         audio_data = np.clip(audio_data, -1.0, 1.0)
 
-
         # Save audio to a file
-        wav_filename = "temp.wav"
-        sf.write(wav_filename, audio_data, 16000)
-        
-        # Display the recorded audio
-        st.audio(wav_filename, format='audio/wav')    
-        mfcc_features = extract_mfcc(wav_filename)
+        with NamedTemporaryFile(delete=True, suffix='.wav') as temp_file:
+            sf.write(temp_file.name, audio_data, 16000)
+            # Display the recorded audio
+            st.audio(temp_file.name, format='audio/wav')
+            mfcc_features = extract_mfcc(temp_file.name)
 
 # Model prediction and display results
 if 'mfcc_features' in locals():
